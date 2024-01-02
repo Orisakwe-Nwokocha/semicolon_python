83fa05d278c227f3ed7202990bb4fc7ecb680628
Orisha <orisakwenwokocha1@gmail.com>
2024-01-02 16:29:10 +0100

    Add files via upload

---
+++ Mr Chibuzo's Snacks/MBTI and Student Grade/mbti_personality_test_app.py
@@ -0,0 +1,9 @@
+from mbti_personality_test import display_questions, display_mbti
+
+name = input("What is your name:\n")
+
+display_questions()
+
+print(f"\nHello {name}, you selected")
+
+display_mbti()
