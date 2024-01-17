87d09d25f08e89f3af34ee9437581c888b2ac2ae
Orisha <orisakwenwokocha1@gmail.com>
2024-01-17 07:02:20 +0100

    Update student_grade_app.py

--- Mr Chibuzo's Snacks/MBTI and Student Grade/student_grade_app.py
+++ Mr Chibuzo's Snacks/MBTI and Student Grade/student_grade_app.py
@@ -1,7 +1,7 @@
 from student_grade import *
 
 # students_grades = [[67, 21, 49], [98, 62, 56], [93, 34, 27], [78, 83, 66]]
-# grades_constructor(students_grades)
+# set_grades(students_grades)
 
-set_grades_list()
+input_grades()
 process_grades()
