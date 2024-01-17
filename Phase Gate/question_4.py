b855f4736db6efa5a693b60edd9925696704256d
Orisha <orisakwenwokocha1@gmail.com>
2024-01-17 19:48:29 +0100

    Add files via upload

---
+++ Phase Gate/question_4.py
@@ -0,0 +1,21 @@
+def sum(x, y):
+	return x + y
+
+def division(x, y):
+	return x / y
+
+def subtraction(x, y):
+	return x - y
+
+def multiplication(x, y):
+	return x * y
+
+def floor_division(x, y):
+	return x // y
+
+
+print(sum(10, 20))
+print(division(10, 20))
+print(subtraction(10, 20))
+print(multiplication(10, 20))
+print(floor_division(10, 20))
