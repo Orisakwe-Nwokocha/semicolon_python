07928f9090861fbc3800bd7fc268640537d33230
Orisha <orisakwenwokocha1@gmail.com>
2024-01-17 19:50:14 +0100

    Add files via upload

---
+++ Phase Gate/question_11.py
@@ -0,0 +1,5 @@
+def add_to_a_list(name1, name2, name3, name4, name5):
+	new_list = [name1, name2, name3, name4, name5]
+	return new_list
+
+print(add_to_a_list("apple", "banana", "mango", "sugarcane", "cherry"))
