b13d9739fe01fbf802bc4a887e1849c326f384cb
Orisha <orisakwenwokocha1@gmail.com>
2024-02-02 01:27:49 +0100

    modified the test to mirror a class based test

--- Mr Chibuzo's Snacks/Phonebook/test_phonebook.py
+++ Mr Chibuzo's Snacks/Phonebook/test_phonebook.py
@@ -1,48 +1,88 @@
-from phonebook import *
+import pytest
+
+from phonebook import Phonebook
 
 
-def test_that_phone_book_can_add_contact():
-    nokia_phonebook = get_contacts()
-    assert len(nokia_phonebook) == 1
-
-    add_contact("Orisha", "08035729982")
-    assert len(nokia_phonebook) == 1
+@pytest.fixture
+def nokia_phonebook():
+    return Phonebook()
 
 
-def test_that_phone_book_can_delete_contact():
-    nokia_phonebook = get_contacts()
-    assert len(nokia_phonebook) == 0
+def test_that_phone_book_can_add_contact(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
 
-    add_contact("Orisha", "08035729982")
-    add_contact("ajiri", "1234")
-    add_contact("izu", "878")
-    assert len(nokia_phonebook) == 3
-
-    delete_contact("ajiri")
-    assert len(nokia_phonebook) == 2
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    assert len(nokia_phonebook.get_contacts()) == 1
 
 
-def test_that_phone_book_search_contact_contact_exists():
-    add_contact("Orisha", "08035729982")
-    assert len(get_contacts()) == 1
+def test_that_phone_book_can_delete_contact(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
 
-    assert search_contact("Orisha") == "Orisha:\n08035729982\n"
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    nokia_phonebook.add_contact("Ajiri", "1234")
+    nokia_phonebook.add_contact("Izu", "878")
+    assert len(nokia_phonebook.get_contacts()) == 3
 
-
-def test_that_phone_book_search_contact_contact_does_not_exist():
-    add_contact("Orisha", "08035729982")
-    assert len(get_contacts()) == 1
-
-    assert search_contact("izu") == "No results for \"izu\""
+    nokia_phonebook.delete_contact("Ajiri")
+    assert len(nokia_phonebook.get_contacts()) == 2
 
 
-def test_view_contacts():
-    assert False
+def test_search_for_an_existing_contact(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    assert len(nokia_phonebook.get_contacts()) == 1
+
+    assert nokia_phonebook.search_contact("Orisha") == "Orisha:\n08035729982\n"
 
 
-def test_edit_one_contact_info():
-    assert False
+def test_search_for_a_nonexistent_contact(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    assert len(nokia_phonebook.get_contacts()) == 1
+
+    assert nokia_phonebook.search_contact("Izu") == "No results for \"Izu\""
 
 
-def test_edit_all_contact_info():
-    assert False
+def test_phone_book_is_not_empty_view_contacts(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    nokia_phonebook.add_contact("Ajiri", "1234")
+    assert len(nokia_phonebook.get_contacts()) == 2
+
+    assert nokia_phonebook.view_contacts() == "Ajiri:\n1234\n\nOrisha:\n08035729982\n\n"
+
+
+def test_phone_book_is_empty_view_contacts(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
+
+    assert nokia_phonebook.view_contacts() == "Phonebook is empty"
+
+
+def test_edit_one_contact_info_only_contact_name_is_edited(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    assert len(nokia_phonebook.get_contacts()) == 1
+    assert nokia_phonebook.search_contact("Orisakwe Nwokocha") == "No results for \"Orisakwe Nwokocha\""
+
+    nokia_phonebook.edit_one_contact_info("Orisha", 1, "Orisakwe Nwokocha")
+    assert nokia_phonebook.search_contact("Orisakwe Nwokocha") == "Orisakwe Nwokocha:\n08035729982\n"
+
+
+def test_edit_one_contact_info_only_phone_number_is_edited(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    assert len(nokia_phonebook.get_contacts()) == 1
+    assert nokia_phonebook.search_contact("Orisha") != "Orisha:\n08125358910\n"
+
+    nokia_phonebook.edit_one_contact_info("Orisha", 2, "08125358910")
+    assert nokia_phonebook.search_contact("Orisha") == "Orisha:\n08125358910\n"
+
+
+def test_edit_all_contact_info_all_info_are_edited(nokia_phonebook):
+    assert len(nokia_phonebook.get_contacts()) == 0
+    nokia_phonebook.add_contact("Orisha", "08035729982")
+    assert len(nokia_phonebook.get_contacts()) == 1
+    assert nokia_phonebook.search_contact("Orisha") != "Orisakwe Nwokocha:\n08125358910\n"
+
+    nokia_phonebook.edit_all_contact_info("Orisha", "Orisakwe Nwokocha", "08035729982", "08125358910")
+    assert nokia_phonebook.search_contact("Orisakwe Nwokocha") == "Orisakwe Nwokocha:\n08125358910\n"
