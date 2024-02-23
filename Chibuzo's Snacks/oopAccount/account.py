07bb4e81ff62ac7a5e06110806a9a0121eee1745
Orisha <orisakwenwokocha1@gmail.com>
2024-02-23 14:29:37 +0100

    account created and fully tested

--- Chibuzo's Snacks/oopAccount/account.py
+++ Chibuzo's Snacks/oopAccount/account.py
@@ -4,7 +4,7 @@ from oopAccount.exceptions.invalid_pin_exception import InvalidPinException
 
 
 class Account:
-    def __init__(self, name, number, pin):
+    def __init__(self, name: str, number: int, pin: str):
         self._validate_pin_format_and_length(pin)
 
         self.__name = name
@@ -13,32 +13,35 @@ class Account:
         self.__pin = pin
 
     @staticmethod
-    def _validate_pin_format_and_length(pin):
+    def _validate_pin_format_and_length(pin: str):
         Account._validate_pin_format(pin)
         Account._validate_pin_length(pin)
 
     @staticmethod
-    def _validate_pin_format(pin):
+    def _validate_pin_format(pin: str):
         digits = pin.isdigit()
         if not digits:
             raise InvalidPinException("PIN must consist of only digit numbers.")
 
     @staticmethod
-    def _validate_pin_length(pin):
+    def _validate_pin_length(pin: str):
         valid = len(pin) == 4
         if not valid:
             raise InvalidPinException("PIN length must be four digits long.")
 
-    def deposit(self, amount):
+    def deposit(self, amount: int):
         self._validate_amount(amount)
+
         self.__balance += amount
 
-    def withdraw(self, amount, pin):
+    def withdraw(self, amount: int, pin: str):
         self._validate_withdraw_operation(amount, pin)
+
         self.__balance -= amount
 
-    def check_balance(self, pin):
+    def check_balance(self, pin: str) -> int:
         self._validate_pin(pin)
+
         return self.__balance
 
     def _validate_withdraw_operation(self, amount, pin):
@@ -46,23 +49,23 @@ class Account:
         self._validate_amount(amount)
         self._ensure_sufficient_funds(amount)
 
-    def _validate_pin(self, pin):
+    def _validate_pin(self, pin: str):
         if self.is_incorrect(pin):
             raise InvalidPinException("PIN provided is not valid.")
 
-    def _ensure_sufficient_funds(self, amount):
+    def _ensure_sufficient_funds(self, amount: int):
         if amount > self.__balance:
             raise InsufficientFundsException("Insufficient funds to perform this operation.")
 
     @staticmethod
-    def _validate_amount(amount):
+    def _validate_amount(amount: int):
         if amount <= 0:
             raise InvalidAmountException("Amount must be greater than zero.")
 
     def get_number(self):
         return self.__number
 
-    def is_incorrect(self, pin):
+    def is_incorrect(self, pin: str):
         return self.__pin != pin
 
     def get_name(self):
--- Chibuzo's Snacks/oopAccount/bank.py
+++ Chibuzo's Snacks/oopAccount/bank.py
@@ -8,41 +8,43 @@ class Bank:
         self.__accounts = []
         self.__lastAccountCreated = 0
 
-    def register_customer(self, first_name, last_name, pin):
+    def register_customer(self, first_name: str, last_name: str, pin: str) -> Account:
         name = first_name + " " + last_name
         number = self.generate_account_number()
+
         account: Account = Account(name, number, pin)
         self.__accounts.append(account)
 
         return account
 
-    def generate_account_number(self):
+    def generate_account_number(self) -> int:
         self.__lastAccountCreated += 1
+
         return self.__lastAccountCreated
 
-    def find_account(self, account_number):
+    def find_account(self, account_number: int) -> Account:
         for account in self.__accounts:
             if account.get_number() == account_number:
                 return account
 
         raise ValueError("Account does not exist.")
 
-    def check_balance(self, account_number, pin):
+    def check_balance(self, account_number: int, pin: str) -> int:
         account: Account = self.find_account(account_number)
 
         return account.check_balance(pin)
 
-    def deposit(self, account_number, amount):
+    def deposit(self, account_number: int, amount: int):
         account: Account = self.find_account(account_number)
 
         account.deposit(amount)
 
-    def withdraw(self, account_number, amount, pin):
+    def withdraw(self, account_number: int, amount: int, pin: str):
         account: Account = self.find_account(account_number)
 
         account.withdraw(amount, pin)
 
-    def transfer(self, sender_account_number, receiver_account_number, amount, pin):
+    def transfer(self, sender_account_number: int, receiver_account_number: int, amount: int, pin: str):
         sender: Account = self.find_account(sender_account_number)
         receiver: Account = self.find_account(receiver_account_number)
 
@@ -53,5 +55,5 @@ class Bank:
         account: Account = self.find_account(account_number)
         if account.is_incorrect(pin):
             raise InvalidPinException("PIN provided is not valid.")
-        
+
         self.__accounts.remove(account)
