49676840142bb0869ad526edcc54000e91a5a4ae
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-03-01 21:49:45 +0100

    more tasks

--- Sikiru's Roasted Corn/march_tasks/task_one.py
+++ Sikiru's Roasted Corn/march_tasks/task_one.py
@@ -1,10 +1,6 @@
 def calculate(d: str):
     temp = list(map(lambda num: f"{((2 * 50 * int(num) / 30) ** 0.5):.0f}", d.split(",")))
-    result = ""
-    for i in temp:
-        result += i + ","
-
-    return result[:-1]
+    return "".join([str(element + ",") for element in temp])[:-1]
 
 
 print(calculate("100,150,180"))
